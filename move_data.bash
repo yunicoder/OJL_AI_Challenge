19d6296417fc0f45732ae42fa1365913b37aabca
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-05-03 05:47:15 +0900

    fix: move_data

--- move_data.bash
+++ move_data.bash
@@ -3,14 +3,17 @@
 
 ### initialize option variables
 SOURCE_DIR="$(dirname $0)/"
+DOWNLOADS="/mnt/c/Users/atsushi/Downloads/"
+WORKSPACE="/mnt/c/Users/atsushi/Documents/workspace/env/ACS/SaiI/"
 
 
 ### move data
-ZIP_FILES=$( find ${SOURCE_DIR} -name "*.zip" )
-for zip_file_name in ${ZIP_FILES}
+ZIP_FILES=$( find ${DOWNLOADS} -name "*.zip" )
+for zip_file_path in ${ZIP_FILES}
 do
-    mv /mnt/c/Users/atsushi/Downloads/${zip_file_name}.zip /mnt/c/Users/atsushi/Documents/workspace/env/ACS/SaiI/data_each_situation/${zip_file_name}.zip
-    unzip ${SOURCE_DIR}/data_each_situation/${zip_file_name}.zip -d ${SOURCE_DIR}/data_each_situation/
-    rm -r ${SOURCE_DIR}/data_each_situation/${zip_file_name}.zip
+    zip_file_name=$( basename ${zip_file_path} )
+    mv ${zip_file_path} ${WORKSPACE}/data_each_situation/
+    unzip ${SOURCE_DIR}/data_each_situation/${zip_file_name} -d ${SOURCE_DIR}/data_each_situation/
+    rm -r ${SOURCE_DIR}/data_each_situation/${zip_file_name}
     rm -r ${SOURCE_DIR}/data_each_situation/${zip_file_name}/.ipynb_checkpoints
 done
--- video.py
+++ video.py
@@ -2,6 +2,7 @@ import argparse
 from moviepy.editor import ImageSequenceClip
 import os
 
+
 def main():
     parser = argparse.ArgumentParser(description='create driving movie.')
     parser.add_argument(
@@ -21,17 +22,17 @@ def main():
 
     image_folder = os.path.dirname(__file__) + '/data_each_situation/' + args.data_folder_name + '/IMG'
 
-    #画像のパスのリストを作成する
-    image_list = [os.path.abspath('{}'.format(image_folder)) + '/' + path for path in sorted(os.listdir(image_folder))]
+    ### 画像のパスのリストを作成する
+    image_list = [os.path.abspath('{}'.format(image_folder)) + '/'
+                  + path for path in sorted(os.listdir(image_folder))]
 
-    #画像パスリストをImageSequenceClipに渡す
+    ### 画像パスリストをImageSequenceClipに渡す
     clip = ImageSequenceClip(image_list, fps=args.fps)
     video_file_name = args.data_folder_name + '.mp4'
     
     print('make movie. file name is {}.mp4'.format(args.data_folder_name))
     clip.write_videofile(video_file_name)
-    
+
+
 if __name__ == '__main__':
     main()
-
-
